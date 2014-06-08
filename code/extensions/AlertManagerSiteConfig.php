513335ba9db902abdf9385ab01f858bbd975ef97
John Milmine <john.milmine@dna.co.nz>
2014-06-09 11:43:20 +1200

    added a better extension

---
+++ code/extensions/AlertManagerSiteConfig.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Adds new global settings.
+ */
+
+class AlertManagerSiteConfig extends DataExtension {
+
+	public static $db = array(
+		'EmailFrom' => 'Varchar',
+	);
+
+	public function updateCMSFields(FieldList $fields) {
+		// emails
+		$fields->addFieldToTab('Root.Emails', new TextField('EmailFrom', 'All emails come from this address:'));
+	}
+}
