c3b1c117b6e682afb896294d5397b469512bf1df
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:39:57 +1300

    Converted to PSR-2

--- code/extensions/AlertManagerSiteConfig.php
+++ code/extensions/AlertManagerSiteConfig.php
@@ -3,14 +3,16 @@
  * Adds new global settings.
  */
 
-class AlertManagerSiteConfig extends DataExtension {
+class AlertManagerSiteConfig extends DataExtension
+{
 
-	public static $db = array(
-		'EmailFrom' => 'Varchar',
-	);
+    public static $db = array(
+        'EmailFrom' => 'Varchar',
+    );
 
-	public function updateCMSFields(FieldList $fields) {
-		// emails
-		$fields->addFieldToTab('Root.Emails', new TextField('EmailFrom', 'All emails come from this address:'));
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        // emails
+        $fields->addFieldToTab('Root.Emails', new TextField('EmailFrom', 'All emails come from this address:'));
+    }
 }
--- code/model/UserAlert.php
+++ code/model/UserAlert.php
@@ -1,15 +1,15 @@
 <?php
-class UserAlert extends DataObject {
+class UserAlert extends DataObject
+{
 
-	private static $db = array(
-		'Content' => 'HTMLText',
-		'Type' =>	'Enum("Red,Green,Orange")'
-	);
+    private static $db = array(
+        'Content' => 'HTMLText',
+        'Type' =>    'Enum("Red,Green,Orange")'
+    );
 
-	private static $has_one = array(
-		'Member' => 'Member'
-	);
+    private static $has_one = array(
+        'Member' => 'Member'
+    );
 
-	private static $default_sort = 'Type';
-
+    private static $default_sort = 'Type';
 }
